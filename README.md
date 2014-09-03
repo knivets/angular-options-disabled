754a901c2cbc429b9985ec9cc84570067ff1b742
Dima Knivets <knivets@gmail.com>
2014-09-03 23:10:34 +0300

    added description, minified version, bug fixes

--- README.md
+++ README.md
@@ -7,6 +7,22 @@ Angular directive for disabling specific options in ng-options directive.
 Installation
 ============
 
+```bower install angular-options-disabled```
 
 How to use
 ==========
+
+Include ```ngOptionsDisabled``` module to your app, for example: ```angular.module('myApp', ['ngOptionsDisabled'])```.
+
+Add ```ng-options-disabled``` attribute to your ```select``` element with following syntax ```condition for item in collection```.
+
+Example
+=======
+
+```html
+<select ng-model="country" ng-options="country.id as country.name for country in countries" ng-options-disabled="country.disabled == true for country in countries">
+```
+
+Credits
+=======
+Originally based on stackoverflow answer by Vikas Gulati. Fixed and improved by Dima Knivets.
--- src/angular-options-disabled.js
+++ src/angular-options-disabled.js
@@ -1,39 +1,41 @@
 angular.module('ngOptionsDisabled', [])
-.directive('optionsDisabled', function($parse) {
+.directive('ngOptionsDisabled', ['$parse', function($parse) {
 	var disableOptions = function(scope, attr, element, data, fnDisableIfTrue) {
 		// refresh the disabled options in the select element.
-		var options = element.find("option");
-		for (var pos = 0, index = 0; pos < options.length; pos++) {
+		var options = element.find('option');
+		var pos = 0, index = 0;
+		while (pos < options.length) {
 			var elem = angular.element(options[pos]);
 			if (elem.val() != "" && data) {
 				var locals = {};
 				locals[attr] = data[index];
-				elem.attr("disabled", fnDisableIfTrue(scope, locals));
+				elem.attr('disabled', fnDisableIfTrue(scope, locals));
 				index++;
 			}
+			pos++;
 		}
 	};
 	return {
 		priority: 0,
 		require: 'ngModel',
-		link: function(scope, iElement, iAttrs, ctrl) {
+		link: function(scope, el, attrs, ctrl) {
 			// parse expression and build array of disabled options
-			var expElements = iAttrs.optionsDisabled.match(
+			var expElements = attrs.ngOptionsDisabled.match(
 				/^\s*(.+)\s+for\s+(.+)\s+in\s+(.+)?\s*/);
 			var attrToWatch = expElements[3];
 			var fnDisableIfTrue = $parse(expElements[1]);
 			scope.$watch(attrToWatch, function(newValue, oldValue) {
 				if(newValue)
-					disableOptions(scope, expElements[2], iElement, 
+					disableOptions(scope, expElements[2], el, 
 						newValue, fnDisableIfTrue);
 			}, true);
 			// handle model updates properly
-			scope.$watch(iAttrs.ngModel, function(newValue, oldValue) {
+			scope.$watch(attrs.ngModel, function(newValue, oldValue) {
 				var disOptions = $parse(attrToWatch)(scope);
 				if(newValue)
-					disableOptions(scope, expElements[2], iElement, 
+					disableOptions(scope, expElements[2], el, 
 						disOptions, fnDisableIfTrue);
 			});
 		}
 	};
-});
+}]);
